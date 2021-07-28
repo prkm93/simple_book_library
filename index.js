89a59f5caea91cff8e22234f3f66c417f5f18d54
prkm93 <aradshub93@gmail.com>
2021-07-28 22:19:27 +0530

    added files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let title = document.querySelector('#bookName');
-let author = document.querySelector('#author');
-let type = document.querySelectorAll('#fiction').value;
-
-console.log("type", type);
+const title = document.querySelector('#bookName');
+const author = document.querySelector('#author');
+const fiction = document.querySelector('#fiction');
+const programming = document.querySelector('#programming');
+const cooking = document.querySelector('#cooking');
+const libraryForm = document.getElementById('libraryForm');
 
 // Book constructor function
 function Book(title, author, type) {
@@ -12,7 +13,33 @@ function Book(title, author, type) {
 }
 
 // Display Constructor
-function Display(){
+function Display(book){
+    console.log(book);
     
 }
 
+// Add methods to display prototype
+
+
+// Add submit event listener
+
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    e.preventDefault();
+    let type = "";
+
+    // console.log("fiction", fiction.checked);
+    // console.log("programming", programming.checked);
+    // console.log("cooking", cooking.checked);
+    if (fiction.checked) {
+        type = fiction.value;
+    } else if (programming.checked) {
+        type = programming.value;
+    } else if (cooking.checked) {
+        type = cooking.value;
+    }
+    
+    let book = new Book(title.value, author.value, fiction.value);
+    Display(book);
+}
