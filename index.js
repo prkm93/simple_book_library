28354bded4da36f024a3de5f61eb580e49775661
prkm93 <aradshub93@gmail.com>
2021-07-29 22:07:19 +0530

    addded prototype methods

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fiction = document.querySelector('#fiction');
 const programming = document.querySelector('#programming');
 const cooking = document.querySelector('#cooking');
 const libraryForm = document.getElementById('libraryForm');
+const table = document.querySelector('.table-body');
 
 // Book constructor function
 function Book(title, author, type) {
@@ -15,12 +16,26 @@ function Book(title, author, type) {
 // Display Constructor
 function Display(book){
     console.log(book);
-    
+    let tr = `<tr>
+                <th scope="row">${2}</th>
+                <td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.type}</td>
+              </tr>`;
+    console.log(table, tr);
+    table.insertRow(tr);
 }
 
 // Add methods to display prototype
+Display.prototype.add = function() {
+    
+} 
 
 
+// Clear Display
+Display.prototype.clear = function() {
+    
+}
 // Add submit event listener
 
 libraryForm.addEventListener('submit', libraryFormSubmit);
@@ -29,9 +44,6 @@ function libraryFormSubmit(e) {
     e.preventDefault();
     let type = "";
 
-    // console.log("fiction", fiction.checked);
-    // console.log("programming", programming.checked);
-    // console.log("cooking", cooking.checked);
     if (fiction.checked) {
         type = fiction.value;
     } else if (programming.checked) {
@@ -40,6 +52,8 @@ function libraryFormSubmit(e) {
         type = cooking.value;
     }
     
-    let book = new Book(title.value, author.value, fiction.value);
-    Display(book);
+    let book = new Book(title.value, author.value, type);
+    let display = new Display();
+    display.add(book);
+    display.clear();
 }
